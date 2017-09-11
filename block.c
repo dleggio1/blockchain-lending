7219e9e7e8b1ee8c49a481b7d6599473e8902ba5
Dylan Leggio <dleggio1@binghamton.edu>
2017-09-10 20:58:05 -0400

    test generating block

--- block.c
+++ block.c
@@ -1,9 +1,27 @@
 #include "block.h"
 
+block* genesisBlock;
 block* latestBlock;
 
 int main(int argc, char **argv)
 {
+	// create hard-coded genesis block
+	genesisBlock = malloc(sizeof(block));
+	genesisBlock->index = 0;
+	genesisBlock->timestamp = time(NULL);
+	genesisBlock->data = "Genesis Block.";
+	genesisBlock->hash = "d8a005a48ea046d008272e3a68e16b406dcd4b8fc40dfc06c2fcb6f46508d04d";
+	genesisBlock->prevHash  = "0";
+	latestBlock = genesisBlock;
+	
+	block* test = generateBlock("dyl");
+	printf("index -> %d\n", test->index);
+	printf("timestamp -> %s", ctime(&(test->timestamp)));
+	printf("data -> %s\n", test->data);
+	printf("hash -> %s\n", test->hash);
+	printf("prevHash -> %s\n", test->prevHash);
+
+
 	return 0;
 }
 
@@ -13,12 +31,12 @@ block* generateBlock(char *data)
 	unsigned int index = latestBlock->index + 1;
 	time_t timestamp = time(NULL);
 	
-	// generate SHA256 hash of block
+	// generate SHA256 hash of block 
 	unsigned char digest[SHA256_DIGEST_LENGTH];
 	SHA256_CTX ctx;
 	SHA256_Init(&ctx);
-	SHA256_Update(&ctx, (unsigned char*) index, strlen((unsigned char*) index));
-	SHA256_Update(&ctx, (unsigned char*) timestamp, strlen((unsigned char*) timestamp));
+	SHA256_Update(&ctx, &index, 1);
+	SHA256_Update(&ctx, ctime(&timestamp), strlen(ctime(&timestamp)));
 	SHA256_Update(&ctx, data, strlen(data));
 	SHA256_Update(&ctx, latestBlock->hash, strlen(latestBlock->hash));
 	SHA256_Final(digest, &ctx);
@@ -27,9 +45,13 @@ block* generateBlock(char *data)
 	block* newBlock = (block*) malloc(sizeof(block));
 	newBlock->index = index;
 	newBlock->timestamp = timestamp;
+	newBlock->data = malloc(strlen(data) + 1);
 	newBlock->data = data;
-	newBlock-> hash = digest;
-	newBlock-> prevHash = latestBlock->hash;
+	newBlock->hash = malloc(strlen(digest) + 1);
+	newBlock->hash = digest;
+	newBlock->prevHash = malloc(strlen(latestBlock->hash) + 1);
+	newBlock->prevHash = latestBlock->hash;
 
+	latestBlock = newBlock;
 	return newBlock;
 }
